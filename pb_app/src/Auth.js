8d2674ab5247a2a7c90a41729c4421f37b946570
KelseyDang2002 <kelseydang813@gmail.com>
2023-03-30 11:32:45 -0700

    login page done

--- pb_app/src/Auth.js
+++ pb_app/src/Auth.js
@@ -2,7 +2,7 @@ import useLogout from "hooks/useLogout";
 import useLogin from "hooks/useLogin";
 import pb from "lib/pocketbase.js";
 import {useForm} from "react-hook-form";
-import "./pb_public/background.jpg";
+import "./pb_public/login_style.css";
 
 export default function Auth() {
     const logout = useLogout();
@@ -10,102 +10,6 @@ export default function Auth() {
     const {register, handleSubmit, reset} = useForm();
 
     const isLoggedIn = pb.authStore.isValid;
-
-    const container = {
-        margin: '0',
-        padding: '0',
-        fontFamily: 'Poppins, sans-serif',
-        boxSizing: 'border-box',
-        width: '100',
-        height: '100vh',
-        backgroundImage: "url('background.jpg')",
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        position: 'relative'
-    }
-
-    const formbox = {
-        width: '90%',
-        maxWidth: '450px',
-        borderRadius: '10px',
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%,-50%)',
-        background: '#fff',
-        padding: '50px 60px 150px',
-        textAlign: 'center'
-    }
-
-    const formboxh1 = {
-        fontSize: '30px',
-        marginBottom: '60px',
-        color: '#3c00a0',
-        position: 'relative'
-    }
-
-    const inputgroup = {
-        height: '200px'
-    }
-
-    const inputfield = {
-        background: '#eaeaea',
-        margin: '15px 0',
-        borderRadius: '3px',
-        display: 'flex',
-        alignItems: 'center',
-        maxHeight: '65%',
-        transition: 'max-height 0.5s',
-        overflow: 'hidden'
-    }
-
-    const input = {
-        width: '100%',
-        background: 'transparent',
-        border: '0',
-        outline: '0',
-        padding: '18px'
-    }
-
-    const loginbtn = {
-        width: '100%',
-        margin: '60px 0px',
-        display: 'flex',
-        position: 'absolute',
-        left: '25%'
-    }
-
-    const loginbtnbutton = {
-        flexBasis: '48%',
-        background: '#eaeaea',
-        color: '#555',
-        height: '40px',
-        borderRadius: '20px',
-        border: '0',
-        outline: '0',
-        marginBottom: '0',
-        cursor: 'pointer',
-        transition: 'background 0.5s'
-    }
-
-    const togglebtn = {
-        width: '20%',
-        display: 'flex',
-        position: 'absolute',
-        left: '40%'
-    }
-
-    const togglebtnbutton = {
-        flexBasis: '100%',
-        background: '#3c00a0',
-        color: '#fff',
-        height: '25px',
-        borderRadius: '3px',
-        border: '0',
-        outline: '0',
-        cursor: 'pointer',
-        transition: 'background 0.5s'
-    }
 
     async function onSubmit(data) {
         login({username: data.username, password: data.password});
@@ -115,8 +19,30 @@ export default function Auth() {
     if (isLoggedIn) 
         return (
             <>
-                <h1 style={formboxh1}>Logged in as: {pb.authStore.model.username}</h1>
-                <button style={loginbtnbutton} onClick={logout}>Log Out</button>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8"></meta>
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+                    <title>Discussion Forum Login Page</title>
+                    <link rel="stylesheet" href="login_page.css"></link>
+                </head>
+                <body>
+                <div class="container">
+                    <div class="form-box">
+                        <h1>Logged in as: {pb.authStore.model.username}</h1>
+
+                        <div class="enter-btn">
+                            <button onClick={logout}>Log Out</button>
+                        </div>
+
+                        <div class="toggle-btn">
+                            <button type="button" id="homebtn">Homepage</button>
+                        </div>
+                    </div>
+                </div>
+                </body>
+                </html>
             </>
         );
 
@@ -131,28 +57,28 @@ export default function Auth() {
                 <link rel="stylesheet" href="login_page.css"></link>
             </head>
             <body>
-            {isLoading && <p>Loading...</p>}
-            {isError && <p style={{color: "red"}}>Invalid Information</p>}
-            <div class="container" style={container}>
-                <div class="form-box" style={formbox}>
-                    <h1 style={formboxh1}>Log In with Email/Username</h1>
+            <div class="container">
+                <div class="form-box">
+                    <h1>Log In with Email/Username</h1>
+                    {isLoading && <p>Loading...</p>}
+                    {isError && <p style={{color: "red"}}>Invalid Information</p>}
                     <form onSubmit={handleSubmit(onSubmit)}>
-                        <div class="input-group" style={inputgroup}>
-                            <div class="input-field" style={inputfield}>
-                                <input type="text" id="emailuser" style={input} placeholder="Email/Username" {...register("username")}/>
+                        <div class="input-group">
+                            <div class="input-field">
+                                <input type="text" id="emailuser" placeholder="Email/Username" {...register("username")}/>
                             </div>
 
-                            <div class="input-field" style={inputfield}>
-                                <input type="password" id="password" style={input} placeholder="Password" {...register("password")}/>
+                            <div class="input-field">
+                                <input type="password" id="password" placeholder="Password" {...register("password")}/>
                             </div>
                         </div>
 
-                        <div class="enter-btn" style={loginbtn}>
-                            <button type="submit" id="loginbtn" style={loginbtnbutton} disabled={isLoading}>{isLoading ? "Loading" : "Login"}</button>
+                        <div class="enter-btn">
+                            <button type="submit" id="loginbtn" disabled={isLoading}>{isLoading ? "Loading" : "Login"}</button>
                         </div>
 
-                        <div class="toggle-btn" style={togglebtn}>
-                            <button type="button" id="signupbtn" style={togglebtnbutton}>Sign Up</button>
+                        <div class="toggle-btn">
+                            <button type="button" id="signupbtn">Sign Up</button>
                         </div>
                     </form>
                 </div>
