f4e8543d8f4ce1d5adfbde247e54533db6cbf904
Kelsey Q. Dang <89491346+KelseyDang2002@users.noreply.github.com>
2023-04-26 08:12:03 -0700

    Delete Discussion.js

--- pb_app/src/Discussion.js
+++
@@ -1,8 +0,0 @@
-import useLogout from "hooks/useLogout";
-import pb from "lib/pocketbase";
-
-export default function Discussion() {
-    const logout = useLogout();
-
-    const isLoggedIn = pb.authStore.isValid;
-}
